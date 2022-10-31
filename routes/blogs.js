c8635412f31c27dd9fce2a0a47dd98759b666c6d
RazorHollow <rob@razorhollow.com>
2022-10-31 12:51:22 -0400

    blog create functionality

---
+++ routes/blogs.js
@@ -0,0 +1,15 @@
+import { Router } from 'express'
+import * as blogsCtrl from '../controllers/blogs.js'
+import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
+
+const router = Router()
+
+// ================ Public Routes ======================
+
+
+
+// ================ Protected Routes ======================
+router.use(decodeUserFromToken)
+router.post('/', checkAuth, blogsCtrl.create)
+
+export { router }
